06b1bd1050512accbf27a568739d2ba6ca982a02
Daniel Shin <danielshin234@gmail.com>
2019-02-27 15:27:21 -0800

    init

---
+++ cmake-build-debug/CMakeFiles/COMP4981_asn2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COMP4981_asn2.dir/main.c.o"
+  "COMP4981_asn2.pdb"
+  "COMP4981_asn2.exe"
+  "libCOMP4981_asn2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/COMP4981_asn2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
