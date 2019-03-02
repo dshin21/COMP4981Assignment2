adf215aeb0b396322f560e5d48ac8ca3316e9975
Daniel Shin <danielshin234@gmail.com>
2019-03-01 23:47:25 -0800

    updated script

--- b4Run.sh
+++ b4Run.sh
@@ -1,2 +1,5 @@
-make clean
+for whatever in `ipcs -s | awk '{print $2}'`; do ipcrm -s $whatever; done;
+for whatever in `ipcs -q | awk '{print $2}'`; do ipcrm -q $whatever; done;
+rm -f *.o *.bak MessageQueueApp
 make
+./MessageQueueApp.exe server
